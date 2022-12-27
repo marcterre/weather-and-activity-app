fcf5c3fd39863a6622141ec8390fd120876c08e2
Marc Terre <marc.terre@outlook.de>
2022-12-27 18:14:17 +0100

    add Entry component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Footer from "./components/Footer";
+import Entry from "./components/Entry";
 
 function App() {
   return (
-    <div className="App">
+    <body>
       <Header />
-      <Form />
+      <main>
+        <Entry />
+        <Form />
+      </main>
       <Footer />
-    </div>
+    </body>
   );
 }
 
