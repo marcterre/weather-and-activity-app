b758ad397f00462eb1571994f04b76204517f689
Marc Terre <marc.terre@outlook.de>
2022-12-27 18:47:19 +0100

    add: basic form style

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,22 +3,33 @@ import styled from "styled-components";
 export default function Form() {
   return (
     <StyledForm>
-      <fieldset>
-        <legend>Add new Activity:</legend>
-        <label htmlFor="name">Name:</label>
+      <StyledFieldset>
+        <StyledLegend>Add new Activity:</StyledLegend>
+        <label htmlFor="name">Name: </label>
         <input
           id="name"
           type="text"
           name="name"
           placeholder="Type your name here..."
         ></input>
-        <label htmlFor="checkbox">Good-weather activity:</label>
+        <label htmlFor="checkbox">Good-weather activity: </label>
         <input type="checkbox" name="checkbox"></input>
-      </fieldset>
+      </StyledFieldset>
     </StyledForm>
   );
 }
 
 const StyledForm = styled.form`
   border: 1px solid black;
+`;
+
+const StyledFieldset = styled.fieldset`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: flex-start;
+`;
+
+const StyledLegend = styled.legend`
+  font-size: 1.5em;
 `;
